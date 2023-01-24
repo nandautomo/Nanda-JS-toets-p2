4e3df471c84263dd48a3f585bdd05b44b876bd6c
nandautomo <302468986@student.rocmondriaan.nl>
2023-01-24 13:24:36 +0100

    helft toets gemaakt

---
+++ main.js
@@ -0,0 +1,30 @@
+//Opdracht 1 
+let x = 21;
+let y = 22;
+
+const result = (x + y);
+
+
+console.log(result); 
+
+
+//Opdracht 2
+
+
+
+//Opdracht 3
+const cars = ['Mercedes', 'Tesla', 'Peugeot', 'Volkswagen', 'Kia', 'Seat']; 
+
+console.log(cars);
+
+
+//Opdracht 4
+function showSum() 
+
+
+
+//Opdracht 5
+
+
+
+//Opdracht 6
